627e4e4fd9184b4f19445118726b2fb3d147cd91
Milton Silva <milton.alexis.sm@gmail.com>
2024-03-31 11:05:36 -0500

    primer commit

---
+++ Plantilla/script.js
@@ -0,0 +1,33 @@
+const getCoupons = async () => {
+  const response = await fetch('https://coupons-hwhz.onrender.com/api/coupons');
+  const coupons = await response.json();
+
+  let couponsText = '';
+
+  for (const coupon of coupons) {
+    couponsText += `<div id="main" class="coupon">
+  <div id="couponLeft" class="couponL">
+    <h6>valido hasta; ${coupon.expirationDate}</h6>
+  </div>
+  <div id="couponRight" class="couponR">
+    <h2>${coupon.description}</h2>
+  </div>
+  <button class="button" onclick='useCoupon(${coupon.id})'>Usar cupon</button>
+</div>`;
+  }
+
+  const mainDiv = document.getElementById('app');
+  mainDiv.innerHTML = couponsText;
+};
+const useCoupon = async (id) => {
+  const response = await fetch(
+    `https://coupons-hwhz.onrender.com/api/coupons/${id}`,
+    {
+      method: 'delete',
+    }
+  );
+  const coupon = await response.json();
+  alert(coupon.message);
+  location.reload();
+};
+getCoupons();
